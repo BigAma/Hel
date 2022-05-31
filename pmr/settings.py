4324149addc9879f00a0fb0f53e9438f6226c806
Amareh ALI BARREH <71445449+BigAma@users.noreply.github.com>
2022-05-31 19:56:56 +0300

    Update settings.py
    Security Updates

--- pmr/settings.py
+++ pmr/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w*8lidw72fj+-am_42&8bf@ahh6gg2h-*u!e11n^6=r^h*q7e!'
+SECRET_KEY = '--------------------------------------------------------'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
